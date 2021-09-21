07b1106cfdfca00a04fdb087b864a283ad85bf61
Sumit Lad <sumitlad@amazon.com>
2021-09-21 01:10:59 -0700

    Updated test data parameters

--- test_weather.py
+++ test_weather.py
@@ -10,7 +10,8 @@ def initial_url():
 	return url
 
 
-@mark.parametrize("input_city", [('San Francisco'), ('New York'), ('London'), ('Sydney')])
+@mark.parametrize("input_city",
+	[('San Francisco'), ('New York'), ('London'), ('Sydney')])
 def test_get_weather_by_city_name(input_city, initial_url):
 	url = initial_url + '&q=' + input_city
 	response = requests.get(url)
@@ -19,7 +20,8 @@ def test_get_weather_by_city_name(input_city, initial_url):
 	assert response_body.get('city').get('name') == input_city
 
 
-@mark.parametrize("input_lat, input_lon", [(37.7749, -122.4194), (-37.7749, 122.4194), (35, 139), (-55.5, -37.5)])
+@mark.parametrize("input_lat, input_lon",
+	[(37.7749, -122.4194), (-37.7749, 122.4194), (35, 139), (-55.5, -37.5)])
 def test_get_weather_by_coordinates(input_lat, input_lon, initial_url):
 	url = initial_url + '&lat=' + str(input_lat) + '&lon=' + str(input_lon)
 	response = requests.get(url)
@@ -29,7 +31,9 @@ def test_get_weather_by_coordinates(input_lat, input_lon, initial_url):
 	assert response_body.get('city').get('coord').get("lon") == input_lon
 
 
-@mark.parametrize("input_zip, expected_city", [(10007, 'New York'), (94105, 'San Francisco')])
+@mark.parametrize("input_zip, expected_city", 
+	[(10007, 'New York'), (94105, 'San Francisco'),
+	(94040, 'Mountain View'), (90808, 'Long Beach')])
 def test_get_weather_by_zipcode(input_zip, expected_city, initial_url):	
 	url = initial_url + '&zip=' + str(input_zip)
 	response = requests.get(url)
